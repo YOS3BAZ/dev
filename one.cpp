26088b1519f4121336d6b6ab0081182f59ea4b31
YOUSEF AL-BAZELI <66056761+YOS3BAZ@users.noreply.github.com>
2024-09-28 12:36:36 +0000

    start

---
+++ one.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int number;
+
+    cout << "Enter Pointer Var => " ;
+    cin >> number;
+
+
+    cout << "\n\n Your Number Is ==> " << endl;
+
+    
+    
+}
